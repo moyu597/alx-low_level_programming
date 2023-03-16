2b2c5fb6eceb1188ae0a37e9205327711c64a935
Moyo <moyu597@yahoo.com>
2023-03-16 14:35:37 -0700

    missed anoher character

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(mychar));
 	printf("Size of an int: %lu byte(s)\n", sizeof(myint));
-	printf("Size of a long: %lu byte(s)\n", sizeof(mylong));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(mylong));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(mylonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(myfloat));
 
