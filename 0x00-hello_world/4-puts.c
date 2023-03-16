9acb0379345bccdeea87e50f40194ea3811eab77
Moyo <moyu597@yahoo.com>
2023-03-16 13:51:33 -0700

    printing message without printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints programming is like building a multilingual puzzle
+ * Return Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char message[] = "Programming is like building a multilingual puzzle";
+
+	puts(message);
+	return (0);
+}
