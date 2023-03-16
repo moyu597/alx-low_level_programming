bd1dc37964261bc44d07d8569cf3fcf984299004
Moyo <moyu597@yahoo.com>
2023-03-16 13:55:20 -0700

    fixing betty warning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,11 @@
 /**
  * main - Entry point
  *
- * prints programming is like building a multilingual puzzle
- * Return Always 0 (Sucess)
+ * Return: Always 0 (Sucess)
  */
 
 int main(void)
 {
-	char message[] = "Programming is like building a multilingual puzzle";
-
-	puts(message);
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
