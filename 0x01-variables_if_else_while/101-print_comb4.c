acecf8c7ab72ec1f4fced9e2e744c8bb2689103c
Moyo <moyu597@yahoo.com>
2023-03-17 15:28:32 -0700

    three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - print combinations of two digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int hundreds;
+	int tens;
+	int ones;
+
+	for (hundreds = 0; hundreds <= 9; hundreds++)
+	{
+		for (tens = hundreds + 1; tens <= 9; tens++)
+		{
+			for (ones = tens + 1; ones <= 10; ones++)
+			{
+				putchar(hundreds + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+
+				if (hundreds != 7 || tens != 8 || ones != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
