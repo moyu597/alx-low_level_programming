4b6e61375286285404858d325e49b1438c1f9840
Moyo <moyu597@yahoo.com>
2023-03-17 15:45:25 -0700

    three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,8 +20,11 @@ int main(void)
 				putchar(hundreds + '0');
 				putchar(tens + '0');
 				putchar(ones + '0');
+				if (hundreds != 7 || tens != 8 || ones != 9)
+				{
 				putchar(',');
 				putchar(' ');
+				}
 			}
 		}
 	}
