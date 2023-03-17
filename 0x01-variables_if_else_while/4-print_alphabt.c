d89526bdaca96665520a43667c4084f03d43672e
Moyo <moyu597@yahoo.com>
2023-03-17 12:59:44 -0700

    prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: on success prints(0)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' || letter != 'q')
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
