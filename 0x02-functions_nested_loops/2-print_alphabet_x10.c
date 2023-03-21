5df83ec31d17e338bff0af0ada66762ee49ca07e
Moyo <moyu597@yahoo.com>
2023-03-21 10:00:26 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - to print alphabet x10
+ * Return:returns void
+*/
+
+void print_alphabet_x10(void)
+{
+	int j;
+
+	while (j <= 10)
+	{
+		print_alphabet();
+		j++;
+	}
+}
