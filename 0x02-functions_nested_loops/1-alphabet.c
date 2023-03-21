9263e6dcd80ad494f4664c25c367c221c2f4416f
Moyo <moyu597@yahoo.com>
2023-03-21 09:09:35 -0700

    printing alphabets in small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - printing alphabet in lowercase
+ *
+ * Return: Always (void)
+ */void print_alphabet()
+{
+	int i = 97;
+	for (i;, i<123; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+}
