1ac87d281baec691f026336d123aa2e9ff029f37
Moyo <moyu597@yahoo.com>
2023-03-21 18:45:32 -0700

    program printing sum of even value terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - sum of even terms for fibonacci not exceeding 4000000
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int prev = 1, curr = 2, next, sum = 2;
+
+	while (curr <= 4000000)
+	{
+		next = prev + curr;
+		prev = cuurr;
+		curr = next;
+
+		if (curr % 2 == 0)
+		{
+			sum += curr;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
